c3071fb61f049d648b408468c75be8e148da400a
takahiro-ny <tube412good@gmail.com>
2019-09-24 18:41:36 +0900

    非同期通信２

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,7 @@
 $(function(){ 
-      function buildHTML(message){
-        if  ( message.image ) {
-            var html =
-              `<div class="message" data-message-id=${message.id}>
+    function buildHTML(message){
+      var image = message.image ? `<img src= ${ message.image }>` : "";
+  var html = `<div class="message" data-message-id=${message.id}>
                 <div class="upper-message">
                   <div class="upper-message__user-name">
                     ${message.user_name}
@@ -16,29 +15,10 @@ $(function(){
                     ${message.content}
                   </p>
                 </div>
-                <img src=${message.image} >
-              </div>`
-            return html;
-          } else {
-            var html =
-             `<div class="message" data-message-id=${message.id}>
-                <div class="upper-message">
-                  <div class="upper-message__user-name">
-                    ${message.user_name}
-                  </div>
-                  <div class="upper-message__date">
-                    ${message.date}
-                  </div>
-                </div>
-                <div class="lower-message">
-                  <p class="lower-message__content">
-                    ${message.content}
-                  </p>
-                </div>
+                ${image}
               </div>`
             return html;
           };
-        }
 $('#new_message').on('submit', function(e) {
   e.preventDefault();
   var formData = new FormData(this);
