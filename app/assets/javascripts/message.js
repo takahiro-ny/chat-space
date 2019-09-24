85f13ccd650c3fda1635d9fef78add829da70c61
takahiro-ny <tube412good@gmail.com>
2019-09-24 16:08:20 +0900

    非同期通信

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,6 +2,7 @@ $(function(){
       function buildHTML(message){
         if  ( message.image ) {
             var html =
+              `<div class="message" data-message-id=${message.id}>
                 <div class="upper-message">
                   <div class="upper-message__user-name">
                     ${message.user_name}
